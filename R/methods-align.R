d5553e048a4d57c80b722fcea102226f60a341b1
Adrian Hirt <100226573+admarhi@users.noreply.github.com>
2025-04-15 17:46:15 +0100

    groundwork for new alignment

--- R/methods-align.R
+++ R/methods-align.R
@@ -5,14 +5,7 @@
 #' @return A \code{ConsortiumMetabolismAlignment} object.
 #' @export
 setMethod("align", "ConsortiumMetabolismSet", function(object) {
-  ### Method to retrieve the Binary matrices from all CM in a CMS?
-
-  # Get the consortium indeces in the list
-  cons_ind <- tibble::tibble(
-    name = purrr::map_chr(object@Consortia, \(x) x@Name)
-  ) |>
-    tibble::rowid_to_column("index")
-
+  # Make a tibble with new indeces for all metabolites (might not be needed)
   all_met <- purrr::map(object@Consortia, \(x) tibble::as_tibble(x@colData)) |>
     purrr::reduce(\(x, y) dplyr::full_join(x, y, by = "met")) |>
     dplyr::rename_with(
@@ -20,10 +13,10 @@ setMethod("align", "ConsortiumMetabolismSet", function(object) {
       .cols = dplyr::starts_with("index")
     ) |>
     dplyr::arrange(.data$met, .locale = "C") |>
+    dplyr::relocate("met") |>
     tibble::rowid_to_column("met_ind")
 
-  # Get all of the tibbles from the consortia and use to calculate a new sparse
-  # matrix for the alignment, based on the new indeces of the metabolites.
+  # Retrieve the edges tibbles from all objects and bind the new indeces for met
   all_edges <-
     purrr::map(object@Consortia, \(x) dplyr::mutate(x@Edges, name = x@Name)) |>
     purrr::map_dfr(\(x) rbind(x)) |>
@@ -32,23 +25,47 @@ setMethod("align", "ConsortiumMetabolismSet", function(object) {
     dplyr::left_join(dplyr::select(all_met, 1:2), by = c(produced = "met")) |>
     dplyr::rename(p_ind_alig = "met_ind")
 
+  # Count the number of consortia for each edge
   n_edges <- all_edges |>
     dplyr::select(13:15) |>
     dplyr::reframe(n = dplyr::n(), .by = c("c_ind_alig", "p_ind_alig"))
 
+  # Make a matrix to create graphs
   n_edges_mat <-
     sparseMatrix(n_edges$c_ind_alig, n_edges$p_ind_alig, x = n_edges$n)
 
-  # This currently creates a simple
+  ### Review this
   graph_list <- purrr::map(
     .x = unique(all_edges$name),
     .f = \(.n)
       all_edges |>
         dplyr::filter(.data$name == .n) |>
-        dplyr::select(14:15) |>
+        dplyr::select(1:2) |>
         igraph::graph_from_data_frame(directed = TRUE)
   ) |>
     purrr::set_names(nm = unique(all_edges$name))
+
+  # Get the consortia's binary matrices, could be method
+  mat_list <- purrr::map(comp_set@Consortia, \(x) assays(x)$Binary)
+
+  # Get vector from 1 to length of the list
+  ll <- seq_len(length(mat_list))
+
+  tb <- dplyr::bind_cols(
+    tidyr::expand_grid(cm1 = mat_list, cm2 = mat_list),
+    tidyr::expand_grid(x = ll, y = ll)
+  ) |>
+    dplyr::filter(x < y) |>
+    dplyr::rowwise() |>
+    dplyr::mutate(overlap_score = bin_mat_overlap(.data$cm1, .data$cm2))
+
+  mat <- sparseMatrix(tb$x, tb$y, x = tb$overlap_score) |>
+    Matrix::as.matrix()
+
+  dend <- mat |>
+    dist(method = "euclidean") |>
+    hclust(method = "complete") |>
+    as.dendrogram()
 
   newConsortiumMetabolismAlignment(
     TreeSummarizedExperiment(
@@ -61,23 +78,7 @@ setMethod("align", "ConsortiumMetabolismSet", function(object) {
 
 
 #' @export
-bin_mat_overlap <- function(cm1, cm2) {
-  # Retrieve the binary matrices
-  bm1 <- assays(cm1)$Binary
-  bm2 <- assays(cm2)$Binary
-
-  # Get the intersection of the metabolites
-  met <- intersect(cm1@Metabolites, cm2@Metabolites)
-
-  # Calculate the intersection
-  int <- bm1[met, met] * bm2[met, met]
-
-  # Calculate the overlap
-  sum(int) / min(sum(bm1), sum(bm2))
-}
-
-#' @export
-bin_mat_overlap2 <- function(bm1, bm2) {
+bin_mat_overlap <- function(bm1, bm2) {
   # Get the intersection of the metabolites
   met <- intersect(rownames(bm1), rownames(bm2))
 
