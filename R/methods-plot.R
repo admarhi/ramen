f1b2dfbdce9ff4029e9914b66c25368cfc6e969e
Adrian Hirt <am.hirt@pm.me>
2025-06-17 20:23:28 +0200

    y axis expansion in plots

--- R/methods-plot.R
+++ R/methods-plot.R
@@ -28,37 +28,69 @@ setMethod(
   }
 )
 
-setMethod("plot", "ConsortiumMetabolismSet", function(x) {
-  if (length(x@Dendrogram) == 0) {
-    stop("Not yet clustered!")
-  }
-  dend <- x@Dendrogram[[1]]
-  node_data <- x@NodeData
+setMethod(
+  "plot",
+  "ConsortiumMetabolismSet",
+  function(x, label_colours = NULL, max_nodes = 20, label_size = 4) {
+    if (length(x@Dendrogram) == 0) {
+      stop("Not yet clustered!")
+    }
+    dend <- x@Dendrogram[[1]]
+    node_data <- x@NodeData |>
+      dplyr::filter(.data$node_id <= max_nodes)
 
-  gg_dend <- dendextend::as.ggdend(dend, labels = TRUE, type = "rectangle")
-  ggplot2::ggplot(gg_dend) +
-    ggplot2::geom_point(
-      data = node_data,
-      ggplot2::aes(x = .data$x, y = .data$y),
-      color = "red",
-      size = 7
-    ) +
-    ggplot2::geom_text(
-      data = node_data,
-      ggplot2::aes(x = .data$x, y = .data$y, label = .data$node_id),
-      color = "white",
-      size = 4,
-      fontface = "bold"
-    ) +
-    ggplot2::scale_y_continuous(
-      expand = c(0, 2)
-    ) +
-    ggplot2::theme(
-      axis.text.x = ggplot2::element_text(
-        size = 2
+    gg_dend <- dendextend::as.ggdend(dend, labels = TRUE, type = "rectangle")
+
+    if (!is.null(label_colours)) {
+      label_tb <- gg_dend$labels |>
+        dplyr::left_join(label_colours, by = "label")
+    } else {
+      label_tb <- gg_dend$labels |>
+        dplyr::mutate(colour = "black")
+    }
+
+    ggplot2::ggplot() +
+      # Dendrogram branches
+      ggplot2::geom_segment(
+        data = gg_dend$segments,
+        ggplot2::aes(x = x, y = y, xend = xend, yend = yend)
+      ) +
+      ggplot2::geom_point(
+        data = node_data,
+        ggplot2::aes(x = .data$x, y = .data$y),
+        color = "red",
+        size = 7
+      ) +
+      ggplot2::geom_text(
+        data = node_data,
+        ggplot2::aes(x = .data$x, y = .data$y, label = .data$node_id),
+        color = "white",
+        size = 4,
+        fontface = "bold"
+      ) +
+      ggplot2::scale_y_continuous(
+        expand = ggplot2::expansion(mult = c(0, 0), add = c(2, 1))
+      ) +
+      ggplot2::theme(
+        axis.text = ggplot2::element_blank(),
+        axis.ticks = ggplot2::element_blank(),
+        axis.title = ggplot2::element_blank(),
+        panel.background = ggplot2::element_blank(),
+        plot.background = ggplot2::element_blank(),
+        panel.grid = ggplot2::element_blank()
+      ) +
+      # ggplot2::ggtitle(x@Name) +
+      ggplot2::geom_text(
+        # manually draw the axis labels (i.e., leaf labels)
+        data = label_tb,
+        ggplot2::aes(x = x, y = y - 0.1, label = label), # y - 2 to push below branches
+        angle = 90,
+        hjust = 1,
+        size = label_size,
+        color = label_tb$colour # vector of colors
       )
-    )
-})
+  }
+)
 
 setMethod("plot", "ConsortiumMetabolismAlignment", function(x, type = NULL) {
   # Plot network graph showing alignment of metabolisms
